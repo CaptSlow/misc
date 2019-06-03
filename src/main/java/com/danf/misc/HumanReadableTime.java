37ee1c0c448518d42740d4b320d78f0268b096ed
CaptSlow <daniel.forrester@live.co.uk>
2019-06-03 22:22:23 +0100

    formatted HumanReadableTime and test

--- src/main/java/com/danf/misc/HumanReadableTime.java
+++ src/main/java/com/danf/misc/HumanReadableTime.java
@@ -1,23 +1,21 @@
 package com.danf.misc;
 
 /**
- * A function which takes a non-negative integer (seconds) as input and
- * returns the time in a human-readable format (HH:MM:SS)
+ * A function which takes a non-negative integer (seconds) as input and returns the time in a
+ * human-readable format (HH:MM:SS)
  *
- * HH = hours, padded to 2 digits, range: 00 - 99
- * MM = minutes, padded to 2 digits, range: 00 - 59
- * SS = seconds, padded to 2 digits, range: 00 - 59
- * The maximum time never exceeds 359999 (99:59:59)
- *
- * You can find some examples in the test fixtures.
+ * HH = hours, padded to 2 digits, range: 00 - 99 MM = minutes, padded to 2 digits, range: 00 - 59
+ * SS = seconds, padded to 2 digits, range: 00 - 59 The maximum time never exceeds 359999
+ * (99:59:59)
  */
 class HumanReadableTime {
 
   static String makeReadable(int seconds) {
-    int hours = seconds/(60*60);
-    int minutes = (seconds-(hours*60*60))/60;
-    int secs = seconds - (minutes*60 + hours*60*60);
-    return String.format("%02d:%02d:%02d",hours,minutes,secs);
+    return String
+        .format("%02d:%02d:%02d",
+            seconds / (60 * 60),
+            seconds / 60 - (seconds / (60 * 60) * 60),
+            seconds % 60);
   }
 
 }
