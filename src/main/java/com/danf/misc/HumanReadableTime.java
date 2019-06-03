9d2512708cc4aade919723041ff62cd6e07537e7
CaptSlow <daniel.forrester@live.co.uk>
2019-06-03 22:25:25 +0100

    tidied again HumanReadableTime

--- src/main/java/com/danf/misc/HumanReadableTime.java
+++ src/main/java/com/danf/misc/HumanReadableTime.java
@@ -14,7 +14,7 @@ class HumanReadableTime {
     return String
         .format("%02d:%02d:%02d",
             seconds / (60 * 60),
-            seconds / 60 - (seconds / (60 * 60) * 60),
+            (seconds / 60) % 60,
             seconds % 60);
   }
 
